16aa961844dca56412da1ab0b26ca16103ae34ca
Mohamed Aymen Azza <mohamedaymenazza@gmail.com>
2024-04-06 17:10:05 +0000

    crud

--- app/Http/Controllers/RateController.php
+++ app/Http/Controllers/RateController.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Rate;
 use Illuminate\Http\Request;
 
 class RateController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $rates = Rate::all();
+        return view('rates.index', compact('rates'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('rates.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'rate' => 'required|numeric',
+            'comment' => 'nullable|string',
+            'client_id' => 'required|exists:clients,id',
+        ]);
+
+        $rate = Rate::create($validatedData);
+        return redirect()->route('rates.index')->with('success', 'Rate created successfully.');
+    }
+
+    // Add other CRUD methods as needed
 }
