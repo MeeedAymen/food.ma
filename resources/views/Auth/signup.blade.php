87bde5a76a40f2cd28418ccf3f195e7eab957e90
Mohamed Aymen Azza <mohamedaymenazza@gmail.com>
2024-04-19 15:24:30 +0100

    auth

---
+++ resources/views/Auth/signup.blade.php
@@ -0,0 +1,96 @@
+@extends('Auth.layout')
+@section('title', 'Sign Up')
+
+
+@section('content')
+    <form class="bg-gray-200 h-screen pt-[5%]">
+        <div class="flex justify-center ">
+            <div
+                class="flex items-center border border-gray-300 w-full max-w-md px-6 mx-auto lg:w-2/6  rounded-xl bg-white drop-shadow-lg ">
+                <div class="flex-1 p-4">
+                    <div class="text-center">
+                        <p class="mt-3 text-3xl font-bold text-blue-500">Sign Up</p>
+                    </div>
+                    <div class="mt-8">
+                        <form>
+                            <div>
+                                <label for="name" class="block mb-2 text-sm text-gray-600">First Name</label>
+                                <input type="text" name="firstName" id="firstName" placeholder="First Name"
+                                    class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring">
+                            </div>
+
+                            <div class="mt-3">
+                                <label for="name" class="block mb-2 text-sm text-gray-600">Last Name</label>
+                                <input type="text" name="lastName" id="lastName" placeholder="Last name"
+                                    class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring">
+                            </div>
+
+                            <div class="mt-3">
+                                <label for="email" class="block mb-2 text-sm text-gray-600">Email Address</label>
+                                <input type="email" name="email" id="email" placeholder="name@gmail.com"
+                                    class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring">
+                            </div>
+
+                            <div class="mt-3">
+                                <label for="email" class="block mb-2 text-sm text-gray-600">Confirme Email</label>
+                                <input type="email" name="email" id="email" placeholder="name@gmail.com"
+                                    class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring">
+                            </div>
+
+                            <div class="mt-3">
+                                <label for="email" class="block mb-2 text-sm text-gray-600">Phone</label>
+                                <input type="email" name="email" id="email" placeholder="Your phone"
+                                    class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring">
+                            </div>
+
+                            <div class="mt-3">
+                                <label for="password" class="block mb-2 text-sm text-gray-600">Password</label>
+                                <input type="password" name="password" id="password" placeholder="Enter your Password"
+                                    class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring">
+                            </div>
+
+                            <div class="flex items-center mt-3 space-x-3 flex justify-center">
+                                <div class="shrink-0">
+                                    <img id='preview_img' class="h-12 w-12 object-cover rounded-full"
+                                        src="{{ asset('storage/images/HappyFood.jpg') }}" alt="azza" />
+                                </div>
+                                <label class="block">
+                                    <span class="sr-only">Choose profile photo</span>
+                                    <input type="file" onchange="loadFile(event)"
+                                        class="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-xl file:text-sm file:font-semibold file:bg-blue-200 file:text-black hover:file:text-white hover:file:bg-blue-600" />
+                                </label>
+                            </div>
+
+                            <div class="mt-3">
+                                <button type="submit"
+                                    class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                                    Sign up
+                                </button>
+                            </div>
+
+                        </form>
+                        <p class="mt-6 text-sm text-center text-gray-400">Login to your account
+                            <a href="{{ route('login') }}" class="text-blue-500 hover:underline">Login</a>.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </form>
+
+    <script>
+        var loadFile = function(event) {
+
+            var input = event.target;
+            var file = input.files[0];
+            var type = file.type;
+            var output = document.getElementById('preview_img');
+            output.src = URL.createObjectURL(event.target.files[0]);
+            output.onload = function() {
+                URL.revokeObjectURL(output.src) // free memory
+            }
+        };
+    </script>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+// Tabnine: explain this code
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+Route::get('/login', function () {
+    return view('Auth/login');
+})->name('login');
+Route::get('/signup', function () {
+    return view('Auth/signup');
+})->name('signup');
+
